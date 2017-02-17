71329322d1efde9fd143a7e67ad8a8eb7eaa2714
emejiasc85 <emejiasc85@gmail.com>
2017-02-17 10:16:54 -0600

    refactor edit view inserting a html boilerplate

--- resources/views/commerces/edit.blade.php
+++ resources/views/commerces/edit.blade.php
@@ -1,8 +1,16 @@
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
 {!! Alert::render() !!}
 <h3>{{ $commerce->name }}</h3>
 {!! Form::model($commerce, ['route' => ['commerces.update', $commerce], 'method' => 'PUT']) !!}
 	@include('commerces.partials.fields')
 	<button type="submit">Guardar</button>
 {!! Form::close() !!}
+
+</body>
+</html>
