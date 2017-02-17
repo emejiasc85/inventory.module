56427c5e2324f3dd1d9e4d70ad807c51728bc542
emejiasc85 <emejiasc85@gmail.com>
2017-02-17 08:03:22 -0600

    add commerce model and create fillable and getter url to edit

---
+++ app/Entities/Commerce.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace EmejiasInventory\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Commerce extends Model
+{
+    protected $fillable = [
+        'id',
+    	'name',
+    	'patent_name',
+    	'patent',
+    	'address',
+    	'phone',
+    	'other_phone',
+    	'nit',
+    	'tax',
+    	'profit',
+    	'logo_path',
+    	'slug'
+    ];
+
+
+    public function setNameAttribute($value)
+    {
+    	$this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+	public function getUrlAttribute()
+	{
+		return route('commerces.edit', [$this, $this->slug]);
+	}
+}
+
+
