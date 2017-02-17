0f751b62af49b489f61212b916c37ef455091956
emejiasc85 <emejiasc85@gmail.com>
2017-02-17 16:32:03 -0600

    change a input type on edit a commerce

--- resources/views/commerces/partials/fields.blade.php
+++ resources/views/commerces/partials/fields.blade.php
@@ -2,9 +2,9 @@
 {!! Field::text('patent_name', ['template' => 'templates.inline']) !!}
 {!! Field::text('patent', ['template' => 'templates.inline']) !!}
 {!! Field::textarea('address', ['rows' => 2, 'template' => 'templates.inline', 'required']) !!}
-{!! Field::text('phone', ['template' => 'templates.inline', 'required']) !!}
-{!! Field::text('other_phone', ['template' => 'templates.inline']) !!}
-{!! Field::text('nit', ['template' => 'templates.inline']) !!}
+{!! Field::number('phone', ['template' => 'templates.inline', 'required']) !!}
+{!! Field::number('other_phone', ['template' => 'templates.inline']) !!}
+{!! Field::number('nit', ['template' => 'templates.inline']) !!}
 {!! Field::text('tax', ['template' => 'templates.inline']) !!}
 {!! Field::text('profit', ['template' => 'templates.inline']) !!}
 {!! Field::file('logo', ['template' => 'templates.inline']) !!}
