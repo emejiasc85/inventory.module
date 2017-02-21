c31355e9e2fedd6bac9ae0f8cd63259bb77d613f
emejiasc85 <emejiasc85@gmail.com>
2017-02-21 11:12:48 -0600

    make model to product_groups table

---
+++ app/Entities/ProductGroup.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EmejiasInventory\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class ProductGroup extends Model
+{
+    protected $fillable = ['name', 'description', 'slug'];
+
+    public function setNameAttribute($value)
+    {
+    	$this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
