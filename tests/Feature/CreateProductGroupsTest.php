5967e5a20ba95ec477d527c24469a6ad9b305dce
emejiasc85 <emejiasc85@gmail.com>
2017-02-21 11:15:29 -0600

    add test on create a product groups

---
+++ tests/Feature/CreateProductGroupsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\FeatureTestCase;
+
+
+class CreateProductGroupsTest extends FeatureTestCase
+{
+    function test_a_user_can_create_a_product_group()
+    {
+    	//when
+        $this->actingAs($this->defaultUser())
+        	->visit(route('product.groups.create'))
+        	->see('Grupos de Productos')
+        	->type('Medicina', 'name')
+        	->type('drogas', 'description')
+        	->press('Guardar');
+         //then
+        $this->seeInDatabase('product_groups', [
+        	'name' => 'Medicina',
+        	'description' => 'drogas',
+        	'slug' => 'medicina'
+        ]);
+
+        $this->see('Grupo agregado correctamente');
+
+    }
+
+    public function test_validate_form()
+    {
+
+    	//when
+        $this->actingAs($this->defaultUser())
+        	->visit(route('product.groups.create'))
+        	->see('Grupos de Productos')
+        	->press('Guardar');
+
+         //then
+        $this->seeErrors([
+        	'name' => 'El campo nombre es obligatorio'
+        ]);
+    }
+}
