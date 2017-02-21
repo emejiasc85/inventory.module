98f9392f58784db9dd03e3111ef6bc55961155ff
emejiasc85 <emejiasc85@gmail.com>
2017-02-20 18:45:54 -0600

    add makes index menu link

--- resources/views/layouts/partials/main-menu.blade.php
+++ resources/views/layouts/partials/main-menu.blade.php
@@ -2,10 +2,14 @@
 
     <nav class="sidebar-nav">
         <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link" href=""><i class="icon-speedometer"></i> Dashboard <span class="badge badge-info">NEW</span></a>
-            </li>
-
+            <li class="nav-item nav-dropdown">
+	            <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-speedometer"></i> Herramientas</a>
+	            <ul class="nav-dropdown-items">
+	                <li class="nav-item">
+	                    <a class="nav-link" href="{{ route('makes.index') }}" target="_top"><i class="icon-badge"></i> Marcas</a>
+	                </li>
+	            </ul>
+	        </li>
         </ul>
     </nav>
 </div>
