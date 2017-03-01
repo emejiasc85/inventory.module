d5d90ca0bd3eb7611697f8828703c3455d4f33ee
emejiasc85 <emejiasc85@gmail.com>
2017-03-01 09:40:41 -0600

    refactor row on view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,18 +8,22 @@
 
 	<div class="row">
 		<div class="col-sm-12">
-            <div class="card row">
+            <div class="card">
                 <div class="card-header">
                     <strong>Productos</strong>
                     <small>Listado</small>
                     <a href="{{ route('products.create') }}" class="btn btn-primary pull-right"><i class="fa fa-plus"></i>Nuevo</a>
                 </div>
-
                 <div class="card-block">
-                      <div class="col-xs-5">
+                      <div class="col-xs-12">
                         @include('products.partials.search')
                       </div>
-                      @include('products.partials.table')
+                      <div class="col-xs-12">
+                        @include('products.partials.table')
+
+                      </div>
+
+
 
                 </div>
                 <div class="card-footer">
