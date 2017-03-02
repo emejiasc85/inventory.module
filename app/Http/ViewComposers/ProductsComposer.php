fe6a0e2d4b71b818fbd84dfcb5994a62a925927f
emejiasc85 <emejiasc85@gmail.com>
2017-03-02 08:15:28 -0600

    pass to view makes array to fill select

--- app/Http/ViewComposers/ProductsComposer.php
+++ app/Http/ViewComposers/ProductsComposer.php
@@ -1,7 +1,7 @@
 <?php
 namespace EmejiasInventory\Http\ViewComposers;
 
-use EmejiasInventory\Entities\{ProductPresentation, ProductGroup, UnitMeasure};
+use EmejiasInventory\Entities\{ProductPresentation, ProductGroup, UnitMeasure, Make};
 
 /**
 *
@@ -10,6 +10,7 @@ class ProductsComposer
 {
 	public function compose($view)
 	{
+		$view->makes = Make::pluck('name', 'id')->toArray();
 		$view->presentations = ProductPresentation::pluck('name', 'id')->toArray();
     	$view->groups = ProductGroup::pluck('name', 'id')->toArray();
     	$view->units  = UnitMeasure::pluck('name', 'id')->toArray();
