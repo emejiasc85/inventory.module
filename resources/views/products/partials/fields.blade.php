20db2fc04db455f80703fe2de9745ce97a8428a3
emejiasc85 <emejiasc85@gmail.com>
2017-03-02 08:14:09 -0600

    add select fields makes

--- resources/views/products/partials/fields.blade.php
+++ resources/views/products/partials/fields.blade.php
@@ -2,6 +2,7 @@
 {!! Field::textarea('description', ['template' => 'templates.inline', 'required', 'rows' => 2]) !!}
 {!! Field::number('barcode', ['template' => 'templates.inline']) !!}
 {!! Field::number('minimum_stock', ['template' => 'templates.inline']) !!}
+{!! Field::select('make_id', $makes, ['template' => 'templates.inline', 'required']) !!}
 {!! Field::select('product_presentation_id', $presentations, ['template' => 'templates.inline', 'required']) !!}
 {!! Field::select('product_group_id', $groups, ['template' => 'templates.inline', 'required']) !!}
 {!! Field::select('unit_measure_id', $units, ['template' => 'templates.inline', 'required']) !!}
