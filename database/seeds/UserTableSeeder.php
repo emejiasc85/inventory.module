44b9ee91c827d955b401eea45d6be1a4eeeec1b8
Tony Garcia <lgarcia-1254@outlook.com>
2017-04-06 19:05:15 -0600

    test

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 class UserTableSeeder extends Seeder {
     /**
      * Run the database seeds.
-     *
+     * preuba de git
      * @return void
      */
     public function run() {
