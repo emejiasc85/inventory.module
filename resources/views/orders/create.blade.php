03271a3411b18b9129a0766b4834c8a6d4fb7110
emejiasc85 <emejiasc85@gmail.com>
2017-04-08 15:07:12 -0600

    add disabled attr on click submit

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -14,7 +14,7 @@
                     <strong>Orden</strong>
                     <small>Nuevo</small>
                 </div>
-				{!! Form::open(['route' => ['orders.store'], 'method' => 'POST', 'class' => 'form-horizontal']) !!}
+				{!! Form::open(['route' => ['orders.store'], 'method' => 'POST', 'class' => 'form-horizontal', 'id' =>'CreateOrderForm']) !!}
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-sm-12">
@@ -24,10 +24,11 @@
                     <!--/.row-->
                 </div>
                 <div class="panel-footer">
-                    <button type="submit" class="btn btn-primary">
+                    <button type="submit" id="Submit" class="btn btn-primary">
                         Siguiente
                         <i class="fa fa-angle-double-right "></i>
                     </button>
+
                     <a href="{{ URL::previous() }}" class="btn btn-link text-danger">
                         <i class="fa fa-ban"></i>
                         Cancelar
@@ -38,6 +39,17 @@
 
         </div>
 	</div>
+@stop
+
+@section('scripts')
+<script>
+  $('#Submit').on('click', function (e) {
+        e.preventDefault();
+        $(this).attr('disabled', 'disabled');
+        $(this).text('Espere...')
+        $('#CreateOrderForm').submit();
+  })
+</script>
 @stop
 
 
