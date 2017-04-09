61da368c62cc213957276550f9d1c187f628ef71
Tony Garcia <lgarcia-1254@outlook.com>
2017-04-09 02:51:10 -0600

    FIx titles front end

--- resources/views/audit/partials/head.blade.php
+++ resources/views/audit/partials/head.blade.php
@@ -1,22 +1,22 @@
     <table class="table">
         <tr>
-            <th class="text-left">Prioridad:</th>
+            <th class="text-left">Estado:</th>
             <td class="text-right">
             {{$audit->status}}
-                <span {!! Html::classes(['label', 'label-danger' => $audit->priority == 'Alta', 'label-warning' => $audit->priority == 'Media', 'label-success' => $audit->priority == 'Baja']) !!}>{{ $audit->priority }}
-                </span>
+                <!-- <span {!! Html::classes(['label', 'label-danger' => $audit->priority == 'Alta', 'label-warning' => $audit->priority == 'Media', 'label-success' => $audit->priority == 'Baja']) !!}>{{ $audit->priority }}
+                </span> -->
             </td>
         </tr>
         <tr>
-            <th class="text-left">Proveedor:</th>
+            <th class="text-left">Auditor:</th>
             <td class="text-right">{{ $audit->user->name }}</td>
         </tr>
         <tr>
-            <th class="text-left">Creado: </th>
+            <th class="text-left">Inicio: </th>
             <td class="text-right">{{ $audit->created_at->format('d-m-y') }}</td>
         </tr>
         <tr>
-            <th class="text-left">Ingresó: </th>
+            <th class="text-left">Finalizo: </th>
             <td class="text-right">
                 @if ($audit->status == 'Ingresado')
                     {{ $audit->updated_at->format('d-m-y') }}
