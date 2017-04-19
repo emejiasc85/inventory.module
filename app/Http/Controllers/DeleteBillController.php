67f7f0cbac64a1a0711fa8408f846569683cbfa6
emejiasc85 <emejiasc85@gmail.com>
2017-04-18 23:50:37 -0600

    fix bug on delete bill without details

--- app/Http/Controllers/DeleteBillController.php
+++ app/Http/Controllers/DeleteBillController.php
@@ -15,25 +15,26 @@ class DeleteBillController extends Controller
     {
         DB::beginTransaction();
         $history = StockHistory::where('order_id', $order->id)->get();
+        if(isset($history)){
+            foreach ($history as $item) {
+                $stock = Stock::findOrFail($item->stock_id);
 
-        foreach ($history as $item) {
-            $stock = Stock::findOrFail($item->stock_id);
-
-            if(!$stock){
-                DB::rollback();
-                Alert::danger('Upps!! a ocurrido un error...')->details('Intente nuevamente, si persiste el problema comuniquese con soporte al 54606633');
-                return redirect()->back();
+                if(!$stock){
+                    DB::rollback();
+                    Alert::danger('Upps!! a ocurrido un error...')->details('Intente nuevamente, si persiste el problema comuniquese con soporte al 54606633');
+                    return redirect()->back();
+                }
+                $stock->stock = $stock->stock + $item->lot;
+                $stock->status = true;
+                $stock->save();
             }
-            $stock->stock = $stock->stock + $item->lot;
-            $stock->status = true;
-            $stock->save();
         }
-        if(!$stock){
+        $destroy = Order::destroy($order->id);
+        if(!$destroy){
             DB::rollback();
             Alert::danger('Upps!! a ocurrido un error...')->details('Intente nuevamente, si persiste el problema comuniquese con soporte al 54606633');
             return redirect()->back();
         }
-        $details = Order::destroy($order->id);
         DB::commit();
 
         Alert::success('Se ha eliminado la factura');
