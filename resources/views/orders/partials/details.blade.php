122b3f751a6fcad91d56b03b09b354ac75b60363
emejiasc85 <emejiasc85@gmail.com>
2017-07-03 11:12:17 -0600

    add profit show

--- resources/views/orders/partials/details.blade.php
+++ resources/views/orders/partials/details.blade.php
@@ -19,7 +19,8 @@
                      <th>Precio Compra</th>
                      <th>Precio Venta</th>
                      <th>Vencimiento</th>
-                     <th class="text-right">Costo Total</th>
+                     <th class="text-right">Total Compra</th>
+                     <th class="text-right">Total Venta</th>
                  </tr>
             </thead>
             <tbody>
@@ -38,12 +39,14 @@
                         <td>Q. {{ $detail->sale_price }}</td>
                         <td>{{ $detail->due_date }}</td>
                         <td class="text-right">Q. {{ $detail->total_purchase}}</td>
+                        <td class="text-right">Q. {{ $detail->lot * $detail->sale_price}}</td>
                     @else
                         <td class="col-xs-1"><input type="text" name="lot[]" min="0" step="1" required class="form-control input-sm" value="{{ $detail->lot }} "></td>
                         <td class="col-xs-1"><input type="text" name="purchase_price[]" min="0" required  step="1" class="form-control input-sm" value="{{ $detail->purchase_price }}"></td>
                         <td class="col-xs-1"><input type="text" name="sale_price[]" min="0" step="1" required  class="form-control input-sm" value="{{ $detail->sale_price }}"></td>
                         <td class="col-xs-1"><input type="date" name="due_date[]" class="form-control input-sm" value="{{ $detail->due_date }}"></td>
                         <td class="text-right"><strong>Q. {{ $detail->total_purchase }}</strong></td>
+                        <td class="text-right"><strong>Q. {{ $detail->lot * $detail->sale_price}}</strong></td>
                     @endif
                 </tr>
             @endforeach
@@ -51,10 +54,28 @@
      </table>
      <div class="row">
          <div class="col-lg-4 col-lg-offset-8 col-sm-5 col-sm-offset-4 recap">
-            <table class="table table-clear">
+            <table class="table table-clear table-striped">
                 <tbody>
                     <tr>
-                        <td class="left"><strong>Costo Total</strong></td>
+                        <td class="right">Total Compra</td>
+                        <td class="right"><strong>Q. {{ $order->total }}</strong></td>
+                    </tr>
+
+                    @php
+                        $sale = 0;
+                    @endphp
+                    @foreach ($order->details as $element)
+                        @php
+                            $var = $element->lot * $element->sale_price;
+                            $sale = $sale + $var;
+                        @endphp
+                    @endforeach
+                    <tr >
+                        <td class="right">Total Venta</td>
+                        <td class="right"><strong>Q. {{ $sale }}</strong></td>
+                    </tr>
+                    <tr style="border-top: 1.5px solid black !important">
+                        <td class="right"><strong>Ganancia</strong></td>
                         <td class="right"><strong>Q. {{ $order->total }}</strong></td>
                     </tr>
                 </tbody>
