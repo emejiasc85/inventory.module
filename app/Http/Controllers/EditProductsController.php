f0e92a3f20af49f93b013d29e444f9cf756a132f
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 08:27:56 -0600

    pass rules to request

--- app/Http/Controllers/EditProductsController.php
+++ app/Http/Controllers/EditProductsController.php
@@ -5,21 +5,16 @@ namespace EmejiasInventory\Http\Controllers;
 use EmejiasInventory\Entities\Product;
 use Illuminate\Http\Request;
 use Styde\Html\Facades\Alert;
+use EmejiasInventory\Http\Requests\EditProductRequest;
 
 class EditProductsController extends Controller
 {
-    public function rules($id)
-    {
-        return [
-            
-        ];
-    }
     public function edit(Product $product, $slug)
     {
     	return view('products.edit', compact('product'));
     }
 
-    public function update(Request $request, Product $product)
+    public function update(EditProductRequest $request, Product $product)
     {
         $this->validate($request, $this->rules($product->id));
     	$product->fill($request->all());
