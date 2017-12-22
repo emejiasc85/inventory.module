4a4015c5ae236fd5dc61f9f02f2dd1e8c292dadb
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 08:54:56 -0600

    add id attribute

--- resources/views/bills/partials/fields.blade.php
+++ resources/views/bills/partials/fields.blade.php
@@ -1,9 +1,11 @@
+
 <div class="col-xs-12 col-sm-5 col-lg-3">
     {!! Field::text('nit', ['style' =>  'z-index: 1 !important', 'label' => 'NIT/DPI']) !!}
 </div>
-<div class="col-xs-12 col-sm-7 col-lg-5">
-    {!! Field::text('name') !!}
+<div class="col-xs-12 col-sm-7 col-lg-4">
+    {!! Field::text('name', null, [ 'id' => 'field_name']) !!}
 </div>
 <div class="col-xs-12 col-sm-12 col-lg-4">
-    {!! Field::text('address') !!}
+    {!! Field::text('address', null, [ 'id' => 'field_address']) !!}
 </div>
+
