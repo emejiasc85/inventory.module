55de9309c3c8a5e678eb9017a24c1236c39b0831
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 09:19:47 -0600

    add function to download stocks using excel

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -4,6 +4,7 @@ namespace EmejiasInventory\Http\Controllers;
 
 use EmejiasInventory\Entities\Stock;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class StocksController extends Controller
 {
@@ -18,8 +19,31 @@ class StocksController extends Controller
             ->dueDate($request->get('from_due'), $request->get('to_due'))
             ->stock($request->get('simbol'), $request->get('stock'))
             ->where('status', true)
-            //->OrderBy('id', 'DESC')
+            ->OrderBy('products.id', 'DESC')
             ->paginate();
         return view('stocks.index', compact('stocks'));
+    }
+    
+    
+    public function download(Request $request)
+    {
+        $stocks = Stock::leftJoin('order_details', 'stocks.order_detail_id', '=', 'order_details.id')
+            ->leftjoin('products', 'order_details.product_id', '=', 'products.id')
+            ->where('warehouse_id', 1)
+            ->order($request->get('order_id'))
+            ->product($request->get('name'))
+            ->productId($request->get('id'))
+            ->dueDate($request->get('from_due'), $request->get('to_due'))
+            ->stock($request->get('simbol'), $request->get('stock'))
+            ->where('status', true)
+            ->OrderBy('products.id', 'DESC')
+            ->get();
+            
+        Excel::create('inventario', function($excel) use($stocks) {
+            $excel->sheet('Inventario', function($sheet) use($stocks) {
+                $sheet->loadView('stocks.partials.table', compact('stocks'));
+            });
+    
+        })->export('xls');
     }
 }
