129385215528d85c927561926f29e6db89b960bc
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 08:27:30 -0600

    rename class

--- app/Http/Controllers/EditProductsController.php
+++ app/Http/Controllers/EditProductsController.php
@@ -11,13 +11,7 @@ class EditProductsController extends Controller
     public function rules($id)
     {
         return [
-            'name'                      => 'required',
-            'description'               => 'required',
-            'minimum_stock'             => 'numeric',
-            'product_presentation_id'   => 'required|exists:product_presentations,id',
-            'product_group_id'          => 'required|exists:product_groups,id',
-            'unit_measure_id'           => 'required|exists:unit_measures,id',
-            'make_id'                   => 'required|exists:makes,id',
+            
         ];
     }
     public function edit(Product $product, $slug)
--- app/Http/Requests/EditProductRequest.php
+++ app/Http/Requests/EditProductRequest.php
@@ -4,7 +4,7 @@ namespace EmejiasInventory\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProductRequest extends FormRequest
+class EditProductRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
