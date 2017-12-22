d6270b2856f8ffe6b1137ace145cc29ec4373f8c
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 15:06:20 -0600

    chance id name

--- resources/views/bills/create.blade.php
+++ resources/views/bills/create.blade.php
@@ -29,7 +29,7 @@
                     <!--/.row-->
                 </div>
                 <div class="panel-footer">
-                    <button type="submit" id="Submit" class="btn btn-primary">
+                    <button type="submit" id="Submit" class="btn btn-primary" >
                         Siguiente
                         <i class="fa fa-angle-double-right "></i>
                     </button>
@@ -50,38 +50,41 @@
 @section('scripts')
     {!! Html::script('js/easy-autocomplete.js') !!}
     <script>
-      $('#Submit').on('click', function (e) {
-            e.preventDefault();
-            $('#btn-cancel').fadeOut(1);
-            $(this).attr('disabled', 'disabled');
-            $(this).text('Espere...')
-            $('#CreateOrderForm').submit();
-      })
-    </script>
-
-    <script>
         $(document).ready(function () {
-            $("#nit").val('');        
-            $("#field_name").val('');
-            $("#field_address").val('');
+
+
+            $('#Submit').on('click', function (e) {
+                    e.preventDefault();
+                    $('#btn-cancel').fadeOut(1);
+                    $(this).attr('disabled', 'disabled');
+                    $(this).text('Espere...')
+                    $('#CreateOrderForm').submit();
+            })
+        
+
+            if( $("#name").val() != '' && $("#address").val() != '' ){
+
+            }
+
+
             $("#nit").change(function(event){
                 var nit = $(this).val();
                 var url =  '/auto-complete/people/'+nit;
                 $.getJSON(url, null, function (result) {
                     if(result.success){
                         $("#EditPeople").removeClass('hidden');
-                        $("#field_name").attr('readonly', 'readonly')
-                        $("#field_address").attr('readonly', 'readonly')
+                        $("#name").attr('readonly', 'readonly')
+                        $("#address").attr('readonly', 'readonly')
                         $("#error").addClass('hidden');
-                        $("#field_name").val(result.people.name);
-                        $("#field_address").val(result.people.address);
+                        $("#name").val(result.people.name);
+                        $("#address").val(result.people.address);
                     }
                     else{
                         $("#error").removeClass('hidden');
-                        $("#field_name").removeAttr('readonly');        
-                        $("#field_address").removeAttr('readonly');
-                        $("#field_name").val('');        
-                        $("#field_address").val('');
+                        $("#name").removeAttr('readonly');        
+                        $("#address").removeAttr('readonly');
+                        $("#name").val('');        
+                        $("#address").val('');
                         
                     }
                 });
@@ -89,8 +92,8 @@
 
             $("#EditPeople").click(function(event){
                 event.preventDefault();
-                $("#field_name").removeAttr('readonly');        
-                $("#field_address").removeAttr('readonly');
+                $("#name").removeAttr('readonly');        
+                $("#address").removeAttr('readonly');
                 $(this).addClass('hidden');
             });
             /*
--- resources/views/bills/partials/fields.blade.php
+++ resources/views/bills/partials/fields.blade.php
@@ -3,9 +3,9 @@
     {!! Field::text('nit', ['style' =>  'z-index: 1 !important', 'label' => 'NIT/DPI']) !!}
 </div>
 <div class="col-xs-12 col-sm-7 col-lg-4">
-    {!! Field::text('name', null, [ 'id' => 'field_name']) !!}
+    {!! Field::text('name') !!}
 </div>
 <div class="col-xs-12 col-sm-12 col-lg-4">
-    {!! Field::text('address', null, [ 'id' => 'field_address']) !!}
+    {!! Field::text('address') !!}
 </div>
 
