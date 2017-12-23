b1b17953ed8fdefd0f85b82566d208f824936470
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 20:52:44 -0600

    show product price and rules onchange preces

--- resources/views/orders/partials/details.blade.php
+++ resources/views/orders/partials/details.blade.php
@@ -18,6 +18,7 @@
                      <th>Cant</th>
                      <th>Precio Compra</th>
                      <th>Precio Venta</th>
+                     <th>Precio Actual</th>
                      <th>Vencimiento</th>
                      <th class="text-right">Total Compra</th>
                      <th class="text-right">Total Venta</th>
@@ -37,6 +38,7 @@
                         <td>{{ $detail->lot }}</td>
                         <td>Q. {{ $detail->purchase_price }}</td>
                         <td>Q. {{ $detail->sale_price }}</td>
+                        <td>Q. {{ $detail->product->price }}</td>
                         <td>{{ $detail->due_date }}</td>
                         <td class="text-right">Q. {{ $detail->total_purchase}}</td>
                         <td class="text-right">Q. {{ $detail->lot * $detail->sale_price}}</td>
@@ -44,6 +46,7 @@
                         <td class="col-xs-1"><input type="text" name="lot[]" min="0" step="1" required class="form-control input-sm" value="{{ $detail->lot }} "></td>
                         <td class="col-xs-1"><input type="text" name="purchase_price[]" min="0" required  step="1" class="form-control input-sm" value="{{ $detail->purchase_price }}"></td>
                         <td class="col-xs-1"><input type="text" name="sale_price[]" min="0" step="1" required  class="form-control input-sm" value="{{ $detail->sale_price }}"></td>
+                        <td class="col-xs-1"><input type="text" name="price[]" min="0" step="1" disabled class="form-control input-sm" value="{{ $detail->product->price }}"></td>
                         <td class="col-xs-1"><input type="date" name="due_date[]" class="form-control input-sm" value="{{ $detail->due_date }}"></td>
                         <td class="text-right"><strong>Q. {{ $detail->total_purchase }}</strong></td>
                         <td class="text-right"><strong>Q. {{ $detail->lot * $detail->sale_price}}</strong></td>
@@ -53,7 +56,15 @@
             </tbody>
      </table>
      <div class="row">
-         <div class="col-lg-4 col-lg-offset-8 col-sm-5 col-sm-offset-7 recap">
+            <div class="col-lg-6 col-sm-5 notice">
+                    <div class="well">
+                        <ul>
+                            <li>Si El Precio de Venta es Mayor al Precio Actual el sistema <strong>Actualizará</strong> al nuevo precio </li>
+                            <li>Si El Precio de Venta es Menor al Precio Actual el sistema <strong>Mantendrá</strong> el precio </li>
+                        </ul>
+                    </div>	
+                </div>
+         <div class="col-lg-4 col-lg-offset-2 col-sm-5 col-sm-offset-2 recap">
             <table class="table table-clear table-striped">
                 <tbody>
                     <tr>
