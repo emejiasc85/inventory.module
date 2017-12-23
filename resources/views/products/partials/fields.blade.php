6cb6f1c1794f6d43e6203eae83a7aafc51c9bf96
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 19:36:37 -0600

    add price field

--- resources/views/products/partials/fields.blade.php
+++ resources/views/products/partials/fields.blade.php
@@ -1,8 +1,9 @@
 {!! Field::text('name', ['template' => 'templates.inline', 'required']) !!}
-{!! Field::textarea('description', ['template' => 'templates.inline', 'required', 'rows' => 2]) !!}
+{!! Field::textarea('description', ['template' => 'templates.inline', 'rows' => 2]) !!}
 {!! Field::number('barcode', ['template' => 'templates.inline']) !!}
 {!! Field::number('minimum_stock', ['template' => 'templates.inline']) !!}
 {!! Field::select('make_id', $makes, ['template' => 'templates.inline', 'required']) !!}
 {!! Field::select('product_presentation_id', $presentations, ['template' => 'templates.inline', 'required']) !!}
 {!! Field::select('product_group_id', $groups, ['template' => 'templates.inline', 'required']) !!}
 {!! Field::select('unit_measure_id', $units, ['template' => 'templates.inline', 'required']) !!}
+{!! Field::text('price','0.00', ['template' => 'templates.inline']) !!}
