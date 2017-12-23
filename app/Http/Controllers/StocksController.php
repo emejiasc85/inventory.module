8db7f9947d04e3894fab8b198b3ece82af5ac94e
emejiasc85 <emejiasc85@gmail.com>
2017-12-22 22:53:56 -0600

    group stoks by product

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -10,8 +10,13 @@ class StocksController extends Controller
 {
     public function index(Request $request)
     {
-        $stocks = Stock::leftJoin('order_details', 'stocks.order_detail_id', '=', 'order_details.id')
+        $stocks = Stock::select('products.id', 'products.name', 'products.slug', 'products.price', 'status')
+            ->selectRaw('SUM(stocks.stock) as stock, makes.name as make, product_groups.name as product_group, product_presentations.name as presentation')
+            ->leftJoin('order_details', 'stocks.order_detail_id', '=', 'order_details.id')
             ->leftjoin('products', 'order_details.product_id', '=', 'products.id')
+            ->leftJoin('makes', 'makes.id', '=', 'products.make_id')
+            ->leftJoin('product_groups', 'product_groups.id', '=', 'products.product_group_id')
+            ->leftJoin('product_presentations', 'product_presentations.id', '=', 'products.product_presentation_id')
             ->where('warehouse_id', 1)
             ->order($request->order_id)
             ->ProductBarcode($request->barcode)
@@ -20,24 +25,32 @@ class StocksController extends Controller
             ->dueDate($request->from_due, $request->to_due)
             ->stock($request->simbol, $request->stock)
             ->where('status', true)
-            ->OrderBy('products.id', 'DESC')
+            ->groupBy('products.id', 'products.name', 'products.slug', 'products.price', 'stocks.status', 'makes.name', 'product_groups.name', 'product_presentations.name')
             ->paginate();
+
+
         return view('stocks.index', compact('stocks'));
     }
     
     
     public function download(Request $request)
     {
-        $stocks = Stock::leftJoin('order_details', 'stocks.order_detail_id', '=', 'order_details.id')
+        $stocks = Stock::select('products.id', 'products.name', 'products.slug', 'products.price', 'status')
+            ->selectRaw('SUM(stocks.stock) as stock, makes.name as make, product_groups.name as product_group, product_presentations.name as presentation')
+            ->leftJoin('order_details', 'stocks.order_detail_id', '=', 'order_details.id')
             ->leftjoin('products', 'order_details.product_id', '=', 'products.id')
+            ->leftJoin('makes', 'makes.id', '=', 'products.make_id')
+            ->leftJoin('product_groups', 'product_groups.id', '=', 'products.product_group_id')
+            ->leftJoin('product_presentations', 'product_presentations.id', '=', 'products.product_presentation_id')
             ->where('warehouse_id', 1)
-            ->order($request->get('order_id'))
-            ->product($request->get('name'))
-            ->productId($request->get('id'))
-            ->dueDate($request->get('from_due'), $request->get('to_due'))
-            ->stock($request->get('simbol'), $request->get('stock'))
+            ->order($request->order_id)
+            ->ProductBarcode($request->barcode)
+            ->product($request->name)
+            ->productId($request->id)
+            ->dueDate($request->from_due, $request->to_due)
+            ->stock($request->simbol, $request->stock)
             ->where('status', true)
-            ->OrderBy('products.id', 'DESC')
+            ->groupBy('products.id', 'products.name', 'products.slug', 'products.price', 'stocks.status', 'makes.name', 'product_groups.name', 'product_presentations.name')
             ->get();
             
         Excel::create('inventario', function($excel) use($stocks) {
