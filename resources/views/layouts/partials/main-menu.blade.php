c8f0b63cb44c03a20114a407401bc237715a506d
emejiasc85 <emejiasc85@gmail.com>
2018-04-05 10:26:29 -0600

    remove black space on menus

--- resources/views/layouts/partials/main-menu.blade.php
+++ resources/views/layouts/partials/main-menu.blade.php
@@ -17,8 +17,8 @@
                 <li>
                     <a href="#"><i class="fa fa-filter"></i><span class="text"> Reportes</span> <span class="indicator"></span></a>
                     <ul>
-                        <li><a href="{{ route('reports.due_dates') }}"><i class="fa fa-calendar-times-o"></i><span class="text"> Proximos a vencer</span></a></li>
-                        <li><a href="{{ route('reports.min_stock') }}"><i class="fa fa-exclamation-triangle"></i><span class="text"> En stock minimo</span></a></li>
+                        <li><a href="{{ route('reports.due_dates') }}"><i class="fa fa-calendar-times-o"></i><span class="text">Proximos a vencer</span></a></li>
+                        <li><a href="{{ route('reports.min_stock') }}"><i class="fa fa-exclamation-triangle"></i><span class="text">    En stock minimo</span></a></li>
                         <li><a href="{{ route('reports.resumen') }}"><i class="fa fa-shopping-cart"></i><span class="text">Movimientos</span></a></li>
                         <li><a href="{{ route('reports.products') }}"><i class="fa fa-cubes"></i><span class="text">Top Productos</span></a></li>
                         <li><a href="{{ route('reports.customers') }}"><i class="fa fa-users"></i><span class="text">Top Clientes</span></a></li>
