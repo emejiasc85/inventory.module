75816c05ef6a454ab86e85f0a87392f44e293643
emejiasc85 <emejiasc85@gmail.com>
2018-08-27 20:06:15 -0600

    add new links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,6 @@ box-shadow: 17px 21px 58px -13px rgba(0,0,0,0.75);
 }
 </style>
     <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
-        <a  href="{{ route('bills.index') }}">
-            <div class="smallstat">
-                <i class="fa fa-inbox info"></i>
-                <span class="value text-info">Caja</span>
-            </div><!--/.smallstat-->
-        </a>
-    </div><!--/.col-->
-    <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
         <a  href="{{ route('bills.create') }}">
             <div class="smallstat">
                 <i class="fa fa-shopping-cart primary"></i>
@@ -25,30 +17,33 @@ box-shadow: 17px 21px 58px -13px rgba(0,0,0,0.75);
         </a>
     </div><!--/.col-->
     <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
-        <a  href="{{ route('cash.registers.index') }}">
+        <a  href="{{ route('bills.index') }}">
             <div class="smallstat">
-                <i class="fa fa-bar-chart-o success"></i>
-                <span class="value text-success">Ventas</span>
-            </div><!--/.smallstat-->
-        </a>
-    </div><!--/.col-->
-    <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
-        <a  href="{{ route('stocks.index') }}">
-            <div class="smallstat">
-                <i class="fa fa-cubes info"></i>
-                <span class="value text-info">Existencias</span>
+                <i class="fa fa-inbox info"></i>
+                <span class="value text-info">Caja</span>
             </div><!--/.smallstat-->
         </a>
     </div><!--/.col-->
 
     <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
-        <a  href="{{ route('orders.index') }}">
+        <a  href="{{  route('products.index') }}">
             <div class="smallstat">
-                <i class="fa fa-truck warning" style="background-color: #95a5a6"></i>
-                <span class="value" style="color: #95a5a6">Pedidos</span>
+                <i class="fa fa-barcode warning" style="background-color:#487eb0"></i>
+                <span class="value" style="color: #487eb0">Productos</span>
             </div><!--/.smallstat-->
         </a>
     </div>
+    <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
+        <a  href="{{ route('stocks.index') }}">
+            <div class="smallstat">
+                <i class="fa fa-cubes info" style="background-color: #3498db"></i>
+                <span class="value text-info" style="color: #3498db">Existencias</span>
+            </div><!--/.smallstat-->
+        </a>
+    </div><!--/.col-->
+
+
+
     <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
         <a  href="{{ route('quotes.create') }}">
             <div class="smallstat">
@@ -57,6 +52,40 @@ box-shadow: 17px 21px 58px -13px rgba(0,0,0,0.75);
             </div><!--/.smallstat-->
         </a>
     </div>
+    <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
+            <a  href="{{ route('cash.registers.index') }}">
+                <div class="smallstat">
+                    <i class="fa fa-bar-chart-o success" style="background-color:#7f8c8d"></i>
+                    <span class="value text-success" style="color:#7f8c8d">Ventas</span>
+                </div><!--/.smallstat-->
+            </a>
+        </div><!--/.col-->
+
+
+
+
+
+
+
+
+    <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
+        <a  href="{{ route('people.index') }}">
+            <div class="smallstat">
+                <i class="fa fa-users primary" style="background-color: #778899"></i>
+                <span class="value" style="color: #778899">Personas</span>
+            </div><!--/.smallstat-->
+        </a>
+    </div>
+    <div class="col-lg-3 col-sm-6 col-xs-6 col-xs-12">
+        <a  href="{{ route('orders.index') }}">
+            <div class="smallstat">
+                <i class="fa fa-truck warning" style="background-color: #95a5a6"></i>
+                <span class="value" style="color: #95a5a6">Pedidos</span>
+            </div><!--/.smallstat-->
+        </a>
+    </div>
+
+
 
 
     <!--/.co
