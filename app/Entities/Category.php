6190a185187d71a18f0e4a9e8ef64ba63dd8e9eb
emejiasc85 <emejiasc85@gmail.com>
2018-08-27 20:00:32 -0600

    add category model

---
+++ app/Entities/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EmejiasInventory\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes;
+    protected $guarded = [];
+
+    //mutators
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = title_case($value);
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+    //accesors
+    public function getEditUrlAttribute()
+    {
+    	return route('categories.edit', $this);
+    }
+
+    public function scopeSearch($query)
+    {
+        return $query->orderByDesc('id')
+            ->name();
+    }
+
+    public function scopeName($query)
+    {
+        return $query->when(request()->has('name'), function($q) {
+            $q->orWhere('name', 'LIKE', '%'.request()->name.'%');
+        });
+    }
+}
