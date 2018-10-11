1e32da8c9ef1aa7d5a2ac82409c458de0f5833fe
emejiasc85 <emejiasc85@gmail.com>
2018-10-11 12:51:15 -0600

    print

--- resources/views/products/partials/printbc.blade.php
+++ resources/views/products/partials/printbc.blade.php
@@ -21,8 +21,8 @@
                 <br>
                 <p style="margin: 0px 0 1px;">{{ $product->name }}</p>
                 <p style="margin: 0px 0 1px;">{{ $product->unit->name }}</p>
-                <p style="text-decoration: line-through; margin: 0px 0 1px;"><strong> Q. {{ $product->price }} </strong></p>
-                <p style="background-color: red; background-image: url('/img/paper.gif'); margin: 0px 0 1px;"><strong> Q. {{ $product->offer_price }} </strong></p>
+                <p style="margin: 0px 0 1px;"><strong>Normal Q. {{ $product->price }} </strong></p>
+                <p style="background-color: red; background-image: url('/img/paper.gif'); margin: 0px 0 1px;"><strong>Descuento Q. {{ $product->offer_price }} </strong></p>
                 <p style="margin: 0px 0 1px;">ID: {{ $product->id }}</p>
                 <p style="margin: 0px 0 1px;" class="text-center">
                     <div class="bcTarget text-center">
