d2fb6bc731a485d4c4a9e8f7e30928e23fa7b8b6
emejiasc85 <emejiasc85@gmail.com>
2018-10-17 17:17:38 -0600

    fix issue on payments

--- app/Entities/Order.php
+++ app/Entities/Order.php
@@ -225,11 +225,11 @@ class Order extends Entity
 
     public function addPayment(int $method, float $amount, string $voucher = null)
     {
-        $payment = $this->payments()->updateOrCreate([
+        $this->payments()->delete();
+        $payment = $this->payments()->create([
             'cash_register_id'  => $this->cash_register_id,
             'order_id'          => $this->id,
             'payment_method_id' => $method,
-        ],[
             'amount' => $amount,
             'voucher' => $voucher
         ]);
